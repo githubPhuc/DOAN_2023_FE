ea549862ebf2adcff024badabe5edbd31a943c05
githubPhuc <0306191061@caothang.edu.vn>
2023-04-11 21:55:42 +0700

    add lỗi rồi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
+
 import { LoginComponent  } from './login/login.component';
-import { ForgotPassworkComponent } from './forgot-passwork/forgot-passwork.component';
+
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LayoutComponent } from './dashboard/layout/layout.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AccountComponent,
     LoginComponent,
-    ForgotPassworkComponent,
     LayoutComponent
    
   ],
